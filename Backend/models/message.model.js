bfda91812adcb13041fd20ec08505495a963908a
vaibhavnimesh1 <vaibhavnimesh1@gmail.com>
2024-05-29 13:28:22 +0530

    model created

---
+++ Backend/models/message.model.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const chatModel = mongoose.Schema(
+  {
+    sender: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    chat: { type: mongoose.Schema.Types.ObjectId, ref: "Chat" },
+    content: {
+      type: String,
+      trim: true,
+    },
+  },
+  { timestaps: true }
+);
+
+module.exports = mongoose.Model("User", chatModel);
