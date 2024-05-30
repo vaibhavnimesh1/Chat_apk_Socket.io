fa9e91994493c36933d4c79153438930b9bc30e3
vaibhavnimesh1 <vaibhavnimesh1@gmail.com>
2024-05-30 19:04:57 +0530

    func add img to cloudinary

--- Frontend/src/components/Authentication/Signup.jsx
+++ Frontend/src/components/Authentication/Signup.jsx
@@ -3,15 +3,76 @@ import { FormControl, FormLabel } from "@chakra-ui/form-control";
 import { Input, InputGroup, InputRightElement } from "@chakra-ui/input";
 import { VStack } from "@chakra-ui/layout";
 import { useState } from "react";
+import { useToast } from "@chakra-ui/react";
+
+import axios from "axios";
 
 const Signup = () => {
   const [show, setShow] = useState(false);
   const handleClick = () => setShow(!show);
 
+  const toast = useToast();
+  const [pic, setPic] = useState("");
+  const [loading, setLoading] = useState(false);
+
   const [name, setName] = useState();
   const [email, setEmail] = useState();
   const [confirmpassword, setConfirmpassword] = useState();
   const [password, setPassword] = useState();
+
+  const submitHandler = async () => {
+    console.log("hiiii");
+  };
+  const postDetails = (pic) => {
+    setLoading(true);
+    if (!pic) {
+      toast({
+        title: "",
+        description: "Please Select An Image",
+        status: "warning",
+        duration: 3000,
+        isClosable: true,
+      });
+      setLoading(false);
+      return;
+    }
+    console.log(pic);
+    if (pic.type === "image/jpeg" || pic.type === "image/png") {
+      const data = new FormData();
+      data.append("file", pic);
+      data.append("upload_preset", "chat_apk");
+      data.append("cloud_name", "dwtaizeop");
+
+      axios
+        .post("https://api.cloudinary.com/v1_1/dwtaizeop/image/upload", data)
+        .then((res) => res.data)
+        .then((data) => {
+          setPic(data.url.toString());
+          console.log(data.url.toString());
+          setLoading(false);
+        })
+        .catch((error) => {
+          console.error("Error uploading image:", error);
+          setLoading(false);
+          toast({
+            title: "Error",
+            description: "Failed to upload image",
+            status: "error",
+            duration: 3000,
+            isClosable: true,
+          });
+        });
+    } else {
+      setLoading(false);
+      toast({
+        title: "",
+        description: "Please Select a valid Image (JPEG or PNG)",
+        status: "warning",
+        duration: 3000,
+        isClosable: true,
+      });
+    }
+  };
 
   return (
     <VStack spacing="5px">
@@ -62,9 +123,19 @@ const Signup = () => {
       </FormControl>
       <FormControl id="pic">
         <FormLabel>Upload your Picture</FormLabel>
-        <Input type="file" p={1.5} accept="image/*" />
+        <Input
+          type="file"
+          p={1.5}
+          accept="image/*"
+          onChange={(e) => postDetails(e.target.files[0])}
+        />
       </FormControl>
-      <Button colorScheme="blue" width="100%" style={{ marginTop: 15 }}>
+      <Button
+        onClick={submitHandler}
+        colorScheme="blue"
+        width="100%"
+        style={{ marginTop: 15 }}
+      >
         Sign Up
       </Button>
     </VStack>
